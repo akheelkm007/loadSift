5bfc05c6a978b21d68ad42cac74114c2cd3c3fca
AKHEEL K M <akheelkm007@gmail.com>
2016-12-19 15:07:54 +0530

    Start point is a normal image

--- load-sift.py
+++ load-sift.py
@@ -1,6 +1,14 @@
 from skimage import io 
+from PIL import Image
+from subprocess import call
 
-image = open('book.key')
+
+image = Image.open('sample.png')
+image.save('sample.pgm')
+
+call("./sift < book.pgm > chck.key", shell=True)
+
+image = open('chck.key')
+
 rv = io.load_sift(image)
-
 print rv
